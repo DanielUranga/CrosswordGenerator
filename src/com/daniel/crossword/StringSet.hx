95bf9ce2603ce544e5baed030b1850cd99ad65be
Daniel Uranga <daniel.uranga@gmail.com>
2015-07-18 20:49:45 -0300

    Switch StringSet to a data structure that will allow better searchs

--- src/com/daniel/crossword/StringSet.hx
+++ src/com/daniel/crossword/StringSet.hx
@@ -1,23 +1,79 @@
 package com.daniel.crossword;
 
-class StringSet {
+import com.daniel.crossword.Char;
 
-	var map : Map<String, Bool>;
+private class StringSetNode {
+
+	public var child : Map<Char, StringSetNode>;
+	public var count : Int;
 
 	public function new() {
-		map = new Map<String, Bool>();
+		this.child = new Map<Char, StringSetNode>();
+		this.count = 0;
+	}
+
+	public inline function get(c : Char) {
+		return child.get(c);
+	}
+
+	public inline function set(c : Char, node : StringSetNode) {
+		return child.set(c, node);
+	}
+
+	public inline function remove(c : Char) {
+		return child.remove(c);
+	}
+
+}
+
+class StringSet {
+
+	var root : StringSetNode;
+
+	public function new() {
+		root = new StringSetNode();
 	}
 
 	public function put(s : String) : Void {
-		map.set(s, true);
+		var itr = root;
+		for (i in 0...s.length) {
+			var c : Char = s.charAt(i);
+			if (itr.get(c)==null) {
+				itr.set(c, new StringSetNode());
+			}
+			itr.count++;
+			itr = itr.get(c);
+		}
 	}
 
 	public function has(s : String) : Bool {
-		return map.get(s)==true;
+		var itr = root;
+		for (i in 0...s.length) {
+			var c : Char = s.charAt(i);
+			if (itr.get(c)==null) {
+				return false;
+			}
+			itr = itr.get(c);	
+		}
+		return true;
 	}
 
 	public function remove(s : String) : Void {
-		map.remove(s);
+		var itr = root;
+		var parent : StringSetNode = null;
+		for (i in 0...s.length) {
+			var c : Char = s.charAt(i);
+			if (itr.get(c)==null) {
+				return;
+			}
+			itr.count--;
+			if (itr.count==0 && parent!=null) {
+				var cPrev : Char = s.charAt(i-1);
+				parent.remove(cPrev);
+			}
+			parent = itr;
+			itr = itr.get(c);
+		}
 	}
 
 	public static function fromArray(words : Array<String>) {
