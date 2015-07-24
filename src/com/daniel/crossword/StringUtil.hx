e9a8b7a352c1486ec52e07baa49fdbae2aca51ae
Daniel Uranga <daniel.uranga@gmail.com>
2015-07-24 15:38:54 -0300

    Improved StringUtil methods, now stores everything as lowercase and removes "\r"

--- src/com/daniel/crossword/StringUtil.hx
+++ src/com/daniel/crossword/StringUtil.hx
@@ -6,35 +6,40 @@ class StringUtil {
 
 	public static function encode(str : String) : String {
 		var ret = "";
-		for (i in 0...str.uLength()) {
-			var c = str.uCharAt(i);
-			ret += switch (c) {
+
+		for (c in str.uIterator()) {
+			ret += switch (c.toString()) {
 				case "á": "0";
 				case "é": "1";
 				case "í": "2";
 				case "ó": "3";
 				case "ú": "4";
 				case "ñ": "5";
-				default: c;
+				default: c.toString().toLowerCase();
 			}
 		}
+
+		ret = ret.split("\r").join("");
+
 		return ret;
 	}
 
 	public static function decode(str : String) : String {
 		var ret = "";
-		for (i in 0...str.uLength()) {
-			var c = str.uCharAt(i);
-			ret += switch (c) {
+		for (c in str.uIterator()) {
+			ret += switch (c.toString()) {
 				case "0": "á";
 				case "1": "é";
 				case "2": "í";
 				case "3": "ó";
 				case "4": "ú";
 				case "5": "ñ";
-				default: c;
+				default: c.toString().toLowerCase();
 			}
 		}
+
+		ret = ret.split("\r").join("");
+
 		return ret;
 	}
 
