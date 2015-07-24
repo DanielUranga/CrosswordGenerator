3feca1a45928b740078a67c9c5326c474ec2e90e
Daniel Uranga <daniel.uranga@gmail.com>
2015-07-24 15:39:29 -0300

    Use int codes from 0 to 32 instead of ascii codes

--- src/com/daniel/crossword/Char.hx
+++ src/com/daniel/crossword/Char.hx
@@ -2,12 +2,26 @@ package com.daniel.crossword;
 
 abstract Char(Int) from Int to Int {
 	
+	static var toStringTable = [
+		"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+		"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+		"y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+	];
+
+	static var fromStringTable = [
+		"a"=>0, "b"=>1, "c"=>2, "d"=>3, "e"=>4, "f"=>5, "g"=>6, "h"=>7, 
+		"i"=>8, "j"=>9, "k"=>10, "l"=>11,	"m"=>12, "n"=>13, "o"=>14, "p"=>15,
+		"q"=>16, "r"=>17, "s"=>18, "t"=>19, "u"=>20, "v"=>21, "w"=>22, "x"=>23,
+		"y"=>24, "z"=>25, "0"=>26, "1"=>27, "2"=>28, "3"=>29, "4"=>30, "5"=>31,
+		"6"=>32, "7"=>33, "8"=>34, "9"=>35
+	];
+
 	inline public function new(i:Int) {
 		this = i;
 	}
 
 	@:from
-	static public function fromString(s:String) {
+	static public function fromString(s:String) : Char {
 		return new Char(s.charCodeAt(0));
 	}
 
