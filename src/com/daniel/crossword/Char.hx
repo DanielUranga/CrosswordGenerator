41cab323ccbaf2f43432f59f5d35f9e60a07bd92
Daniel Uranga <daniel.uranga@gmail.com>
2015-08-04 00:27:48 -0300

    "*" Char

--- src/com/daniel/crossword/Char.hx
+++ src/com/daniel/crossword/Char.hx
@@ -23,18 +23,19 @@ abstract Char(Int) from Int to Int {
 	static var toStringTable = [
 		"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
 		"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
-		"y", "z", "0", "1", "2", "3", "4", "5"
+		"y", "z", "0", "1", "2", "3", "4", "5", "*"
 	];
 
 	static var fromStringTable = [
 		"a"=>0, "b"=>1, "c"=>2, "d"=>3, "e"=>4, "f"=>5, "g"=>6, "h"=>7,
 		"i"=>8, "j"=>9, "k"=>10, "l"=>11,	"m"=>12, "n"=>13, "o"=>14, "p"=>15,
 		"q"=>16, "r"=>17, "s"=>18, "t"=>19, "u"=>20, "v"=>21, "w"=>22, "x"=>23,
-		"y"=>24, "z"=>25, "0"=>26, "1"=>27, "2"=>28, "3"=>29, "4"=>30, "5"=>31
+		"y"=>24, "z"=>25, "0"=>26, "1"=>27, "2"=>28, "3"=>29, "4"=>30, "5"=>31,
+		"*"=>32
 	];
 
 	public static inline var firstChar : Char = 0;
-	public static inline var lastChar : Char = 31;
+	public static inline var lastChar : Char = 32;
 
 	inline public function new(i:Int) {
 		this = i;
